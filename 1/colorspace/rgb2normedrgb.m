936e90db89e424cc7540f0944ffa87dd72a95134
Nikita Tokovenko <ntokoven@gmail.com>
2019-02-20 10:59:36 +0100

    Add files via upload
    git commit of all the code for the 2 task of the assignment 1

--- 1/colorspace/rgb2normedrgb.m
+++ 1/colorspace/rgb2normedrgb.m
@@ -1,5 +1,10 @@
 function [output_image] = rgb2normedrgb(input_image)
 % converts an RGB image into normalized rgb
-
+[R, G, B] = getColorChannels(input_image);
+intensity = R + G + B;
+r = R ./ intensity;
+g = G ./ intensity;
+b = B ./ intensity;
+output_image = reshape([r, g, b], size(input_image));
 end
 
