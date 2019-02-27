f0ab4a138578c95a2c8189034599c7d45eeb38c8
Maurice Frank <maurice.frank@posteo.de>
2019-02-27 12:12:53 +0100

    Updated compute log

--- 2/Part-1: Image Enhancement/compute_LoG.m
+++ 2/Part-1: Image Enhancement/compute_LoG.m
@@ -4,7 +4,7 @@ switch LOG_type
     case 1
         %method 1
         K1 = fspecial('gaussian', 5, 0.5);
-        K2 = fspecial('laplcian');
+        K2 = fspecial('laplacian');
         imOut = imfilter(imfilter(image, K1), K2);
     case 2
         %method 2
@@ -12,8 +12,10 @@ switch LOG_type
         imOut = imfilter(image, K);
     case 3
         % method 3
-        
-        
+        %K1 = fspecial('gaussian', 5, 0.8);
+        %K2 = fspecial('gaussian', 5, 0.5);
+        %imOut = imfilter(image, K1) - imfilter(image, K2);
+        imOut = imgaussfilt(image, 0.8) - imgaussfilt(image, 0.5);
 end
 end
 
