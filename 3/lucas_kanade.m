31a40d2132a146d18f440a5a9e2e9e63b96c76ed
vovamedentsiy <30370610+vovamedentsiy@users.noreply.github.com>
2019-03-05 15:01:44 +0100

    Add files via upload

--- 3/lucas_kanade.m
+++ 3/lucas_kanade.m
@@ -1,53 +1,41 @@
-function [Vx,Vy] = lucas_kanade(im1, im2)
-    oim1 = im1;
-    oim2 = im2;
-    if (size(im1,3) == 3)
-        im1 = rgb2gray(im1);
-    end
-    if (size(im2,3) == 3)
-        im2 = rgb2gray(im2);
-    end
+function LK = lucas_kanade(image)
+I = im2double(rgb2gray(imread(image)));
+N = size(I);
 
-    im1 = im2double(im1);
-    im2 = im2double(im2);
-    
-    assert(all(size(im1) == size(im2)))
-    
-    ws = 15;
-    hws = floor(ws/2);
-    [w,h] = size(im1);
-    
-    % Calculate the derivatives
-    Ky = fspecial('prewitt');
-    Kx = Ky';
-    Ix = imfilter(im1, Kx, 'conv', 'symmetric') + imfilter(im2, Kx, 'conv', 'symmetric');
-    Iy = imfilter(im1, Ky, 'conv', 'symmetric') + imfilter(im2, Ky, 'conv', 'symmetric');
-    It = imfilter(im1, 0.25*ones(2), 'conv', 'symmetric') + imfilter(im2, 0.25*ones(2), 'conv', 'symmetric');
-    
-    Vx = zeros(size(im1));
-    Vy = zeros(size(im1));
-    for x = hws+1:ws:w-hws
-        for y = hws+1:ws:h-hws
-            cIx = Ix(x-hws:x+hws, y-hws:y+hws)';
-            cIy = Iy(x-hws:x+hws, y-hws:y+hws)';
-            cIt = It(x-hws:x+hws, y-hws:y+hws)';
+sobel_kernel1 = [1, 0, -1; 2, 0, -2; 1, 0, -1];
+sobel_kernel2 = [1, 2, 1; 0, 0, 0; -1, -2, -1];
+
+
+fix(N(1)/ 15);
+for i=1:fix(N(1)/ 15)
+    for j=1:fix(N(2)/ 15)
+        %imshow(I(15*(i-1)+1:15*i,15*(j-1)+1:15*j))
+        regions( (i-1)*(fix(N(2)/ 15))+j, :, :) = I(15*(i-1)+1:15*i,15*(j-1)+1:15*j);
+        region = I(15*(i-1)+1:15*i,15*(j-1)+1:15*j);
+        
+        Ix = double(imfilter(region, sobel_kernel1));
+        Ix = reshape(Ix, [15*15, 1]);
+        A(:, 1) = Ix;
+        
+        Iy = double(imfilter(region, sobel_kernel2));
+        Iy = reshape(Iy, [15*15, 1]);
+        A(:, 2) = Iy;
+        
+        b = -imfilter(region, 0.25*ones(2), 'conv', 'symmetric');
+        b = reshape(b, [15*15, 1]);
+        %u( (i-1)*(fix(N(2)/ 15))+j, :, :) = pinv(A) * b;
+        %u( (i-1)*(fix(N(2)/ 15))+j, :, :) = 
+        
+        u = mldivide(A,b);
+        
+        Vx(i, j) = u(1);
+        Vy(i, j) = u(2);
             
-            cIx = cIx(:);
-            cIy = cIy(:);
-            
-            A = [cIx, cIy];
-            b = -cIt(:);
-            u = pinv(A) * b;
-            Vx(x,y) = u(1);
-            Vy(x,y) = u(2);
-        end
+        
     end
-    
-    figure(1)
-    imshow(oim1)
-    hold on;
-    quiver(Vx, Vy, 10);
-    hold off;
-    figure(2)
-    imshow(oim2)
+end
+
+quiver(Vx, Vy)
+LK = [Vx, Vy];
+
 end
