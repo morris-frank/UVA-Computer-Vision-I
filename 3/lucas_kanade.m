5e8f230e84c67cf150aeae5822c87d992f447f56
Maurice Frank <maurice.frank@posteo.de>
2019-03-05 17:46:24 +0100

    updated lucas kanade

--- 3/lucas_kanade.m
+++ 3/lucas_kanade.m
@@ -13,21 +13,23 @@ function [Vx,Vy] = lucas_kanade(im1, im2)
     
     assert(all(size(im1) == size(im2)))
     
+    stride = 10;
     ws = 15;
     hws = floor(ws/2);
     [w,h] = size(im1);
     
     % Calculate the derivatives
-    Ky = fspecial('prewitt');
+    %Ky = fspecial('prewitt');
+    Ky = [-1 -1;1 1];
     Kx = Ky';
     Ix = imfilter(im1, Kx, 'conv', 'symmetric') + imfilter(im2, Kx, 'conv', 'symmetric');
     Iy = imfilter(im1, Ky, 'conv', 'symmetric') + imfilter(im2, Ky, 'conv', 'symmetric');
-    It = imfilter(im1, 0.25*ones(2), 'conv', 'symmetric') + imfilter(im2, 0.25*ones(2), 'conv', 'symmetric');
+    It = imfilter(im1, ones(2), 'conv', 'symmetric') - imfilter(im2, ones(2), 'conv', 'symmetric');
     
     Vx = zeros(size(im1));
     Vy = zeros(size(im1));
-    for x = hws+1:ws:w-hws
-        for y = hws+1:ws:h-hws
+    for x = hws+1:stride:w-hws
+        for y = hws+1:stride:h-hws
             cIx = Ix(x-hws:x+hws, y-hws:y+hws)';
             cIy = Iy(x-hws:x+hws, y-hws:y+hws)';
             cIt = It(x-hws:x+hws, y-hws:y+hws)';
@@ -37,7 +39,8 @@ function [Vx,Vy] = lucas_kanade(im1, im2)
             
             A = [cIx, cIy];
             b = -cIt(:);
-            u = pinv(A) * b;
+            %u = pinv(A) * b;
+            u = pinv(A'*A)*A'*b;
             Vx(x,y) = u(1);
             Vy(x,y) = u(2);
         end
