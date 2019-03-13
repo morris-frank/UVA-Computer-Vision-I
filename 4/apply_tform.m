b12e6076504d739a1203dc4bb6df8febd863d6fd
Maurice Frank <maurice.frank@posteo.de>
2019-03-13 18:03:31 +0100

    Updated transform code

--- 4/apply_tform.m
+++ 4/apply_tform.m
@@ -3,12 +3,18 @@ function [transformed_image] = apply_tform(image, tform, method)
     
     if method == 1
         [h,w] = size(image);
-        rot = pinv(reshape(tform(1:4), [2,2]));
-        trans = [tform(6);tform(5)];
-        transformed_image = zeros(h,w);
-        for i = 1:h
-            for j = 1:w
-                nx = round((rot * [i; j]) - trans);
+        
+        corners = [1,1; h,1; 1,w; h,w]';
+        rot = reshape(tform(1:4), [2,2]);
+        irot = pinv(rot);
+        rot_corners = rot * corners;
+    
+        sizetr = ceil(max(rot_corners') - min(rot_corners'));
+        transformed_image = zeros(sizetr(1), sizetr(2));
+        offset = min(rot_corners')';
+        for i = 1:sizetr(1)
+            for j = 1:sizetr(2)
+                nx = round(irot * ([i;j] + offset));
                 if all(nx > 0) && nx(1)<h && nx(2)<w
                     transformed_image(i,j) = image(nx(1), nx(2));
                 end
